55720dd8018890f2d248078d2d40d88c91bec89c
Christian Brown <54856120+christianjbrown@users.noreply.github.com>
2022-07-23 16:41:37 +0100

    Update

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use GuzzleHttp\Psr7\Response;
+
+function run(ServerRequestInterface $request): ResponseInterface
+{
+    $headers = [
+        'Access-Control-Allow-Origin' => '*',
+        'Access-Control-Allow-Methods' => 'GET, OPTIONS',
+        'Content-Type' => 'application/json; charset=utf-8',
+    ];
+
+    try {
+        $bodyJson = [
+            'version' => getenv('K_REVISION'),
+            'timestamp' => time(),
+        ];
+        $body = json_encode($bodyJson, JSON_THROW_ON_ERROR);
+        $response = new Response(200, $headers, $body);
+    } catch (Throwable $e) {
+        $response = new Response(500, $headers, 'An error occurred :(');
+    }
+
+    return $response;
+}
