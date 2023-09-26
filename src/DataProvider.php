31c69b810e9e5c30c7362b304df72a98c2c35027
Christian Brown <54856120+christianjbrown@users.noreply.github.com>
2023-09-26 23:20:40 +0100

    Updates

--- src/DataProvider.php
+++ src/DataProvider.php
@@ -3,66 +3,26 @@
 declare(strict_types=1);
 
 use ChristianBrown\CloudFunction\DataProviderInterface;
+use ChristianBrown\MetOffice\DataPoint\Forecast\ThreeHourlySiteForecastApi;
 use Psr\Http\Message\ServerRequestInterface;
 
 final class DataProvider implements DataProviderInterface
 {
     private ConfigTransformerInterface $configTransformer;
+    private DataTransformerInterface $dataTransformer;
 
     public function __construct()
     {
         $this->configTransformer = new ConfigTransformer();
+        $this->dataTransformer = new DataTransformer();
     }
 
     public function getData(array $env, ServerRequestInterface $request): array
     {
-        $data = [];
-
         $config = $this->configTransformer->transform($env);
-
-        $time = time();
-        $roundedDownTo3Hrs = ($time - ($time % 10800));
-        $roundedUpTo3Hrs = ($roundedDownTo3Hrs + 10800);
-        $roundedDownDiff = $time - $roundedDownTo3Hrs;
-        $roundedUpDiff = $roundedUpTo3Hrs - $time;
-        if ($roundedDownDiff > $roundedUpDiff) {
-            $adjustedTime = $roundedUpTo3Hrs;
-        } else {
-            $adjustedTime = $roundedDownTo3Hrs;
-        }
-
-        $date = gmdate('Y-m-d\TH\Z', $adjustedTime);
-
-        $metUrl = sprintf('http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/%s?res=3hourly&time=%s&key=%s', $config->getSiteId(), $date, $config->getApiKey());
-        $rawMetData = file_get_contents($metUrl);
-        $metData = json_decode($rawMetData, true, 512, \JSON_THROW_ON_ERROR);
-        if (is_array($metData) && !empty($metData['SiteRep']['DV']['Location']['Period']['Rep']) && is_array($metData['SiteRep']['DV']['Location']['Period']['Rep'])) {
-            $weatherData = $metData['SiteRep']['DV']['Location']['Period']['Rep'];
-            $tempFeelsLike = null;
-            $temp = null;
-            $humidity = null;
-            $precipitation = null;
-            if (isset($weatherData['F']) && is_numeric($weatherData['F'])) {
-                $tempFeelsLike = (float) $weatherData['F'];
-            }
-            if (isset($weatherData['T']) && is_numeric($weatherData['T'])) {
-                $temp = (float) $weatherData['T'];
-            }
-            if (isset($weatherData['H']) && is_numeric($weatherData['H'])) {
-                $humidity = (int) $weatherData['H'];
-            }
-            if (isset($weatherData['Pp']) && is_numeric($weatherData['Pp'])) {
-                $precipitation = (int) $weatherData['Pp'];
-            }
-            $data = [
-                'temp_feels_like' => $tempFeelsLike,
-                'temp' => $temp,
-                'humidity' => $humidity,
-                'precipitation' => $precipitation,
-                'valid_from' => $adjustedTime,
-                'valid_to' => $adjustedTime + 10800,
-            ];
-        }
+        $threeHourlyForecastApi = new ThreeHourlySiteForecastApi($config->getApiKey());
+        $forecast = $threeHourlyForecastApi->getOnePeriod($config->getSiteId());
+        $data = $this->dataTransformer->transform($forecast);
 
         return $data;
     }
---
+++ src/DataTransformer.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+use ChristianBrown\MetOffice\DataPoint\Forecast\Model\Forecast;
+use ChristianBrown\MetOffice\DataPoint\Forecast\Model\ForecastLocationPeriod;
+use ChristianBrown\MetOffice\DataPoint\Forecast\Model\ForecastLocationShortPeriodRepresentation;
+
+final class DataTransformer implements DataTransformerInterface
+{
+    public function transform(Forecast $forecast): array
+    {
+        $data = [];
+        $periods = $forecast->location->periods;
+        if (isset($periods[0]) && $periods[0] instanceof ForecastLocationPeriod) {
+            $period = $periods[0];
+            $reps = $period->representations;
+            if (isset($reps[0]) && $reps[0] instanceof ForecastLocationShortPeriodRepresentation) {
+                $rep = $reps[0];
+
+                $day = strtotime($period->value);
+                $validFrom = $day + ($rep->timePeriod * 60);
+                $validTo = $day + ($rep->timePeriod * 60) + (3 * 60 * 60);
+
+                $data = [
+                    self::RESPONSE_KEY_FEELS_LIKE => $rep->feelsLike,
+                    self::RESPONSE_KEY_MAX_UV_INDEX => $rep->maxUvIndex,
+                    self::RESPONSE_KEY_PRECIPITATION_PROBABILITY => $rep->precipitationProbability,
+                    self::RESPONSE_KEY_SCREEN_RELATIVE_HUMIDITY => $rep->screenRelativeHumidity,
+                    self::RESPONSE_KEY_TEMPERATURE => $rep->temperature,
+                    self::RESPONSE_KEY_VALID_FROM => $validFrom,
+                    self::RESPONSE_KEY_VALID_FROM_ISO8601 => date('c', $validFrom),
+                    self::RESPONSE_KEY_VALID_TO => $validTo,
+                    self::RESPONSE_KEY_VALID_TO_ISO8601 => date('c', $validTo),
+                    self::RESPONSE_KEY_VISIBILITY => $rep->visibility,
+                    self::RESPONSE_KEY_WEATHER_TYPE => $rep->weatherType,
+                    self::RESPONSE_KEY_WIND_DIRECTION => $rep->windDirection,
+                    self::RESPONSE_KEY_WIND_GUST => $rep->windGust,
+                    self::RESPONSE_KEY_WIND_SPEED => $rep->windSpeed,
+                ];
+            }
+        }
+
+        return $data;
+    }
+}
