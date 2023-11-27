246c662a73088511bdbe5e2d9d3c5445c64a82e4
Christian Brown <54856120+christianjbrown@users.noreply.github.com>
2023-11-27 00:47:24 +0000

    Revert "Updates"
    This reverts commit 89d9f2cd28a338058abfc3de1cf4ccccd7bb82b6.

--- index.php
+++ index.php
@@ -4,30 +4,31 @@ declare(strict_types=1);
 
 date_default_timezone_set('UTC');
 
-use ChristianBrown\CloudFunction\CloudFunction;
-use ChristianBrown\CloudFunction\FunctionConfigTransformer;
-use ChristianBrown\GetMetOfficeTemps\ConfigTransformer;
-use ChristianBrown\GetMetOfficeTemps\DataProvider;
-use ChristianBrown\GetMetOfficeTemps\ForecastTransformer;
-use ChristianBrown\MetOffice\DataPoint\Forecast\ThreeHourlySiteForecastApi;
-use ChristianBrown\MetOffice\DataPoint\Forecast\Transformer\WeatherTypeTransformer;
+//use ChristianBrown\CloudFunction\CloudFunction;
+//use ChristianBrown\CloudFunction\FunctionConfigTransformer;
+//use ChristianBrown\GetMetOfficeTemps\ConfigTransformer;
+//use ChristianBrown\GetMetOfficeTemps\DataProvider;
+//use ChristianBrown\GetMetOfficeTemps\ForecastTransformer;
+//use ChristianBrown\MetOffice\DataPoint\Forecast\ThreeHourlySiteForecastApi;
+//use ChristianBrown\MetOffice\DataPoint\Forecast\Transformer\WeatherTypeTransformer;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 function run(ServerRequestInterface $request): ResponseInterface
 {
-    $env = getenv();
-    $functionConfigTransformer = new FunctionConfigTransformer();
-    $configTransformer = new ConfigTransformer($functionConfigTransformer);
-    $config = $configTransformer->transform($env);
-
-    $weatherTypeTransformer = new WeatherTypeTransformer();
-    $forecastTransformer = new ForecastTransformer($weatherTypeTransformer);
-    $threeHourlyForecastApi = new ThreeHourlySiteForecastApi($config->getApiKey());
-
-    $dataProvider = new DataProvider($config, $forecastTransformer, $threeHourlyForecastApi);
-    $cloudFunction = new CloudFunction($dataProvider, $config->getFunctionConfig());
-    $response = $cloudFunction->run($request);
-
-    return $response;
+    die('Hello World');
+//    $env = getenv();
+//    $functionConfigTransformer = new FunctionConfigTransformer();
+//    $configTransformer = new ConfigTransformer($functionConfigTransformer);
+//    $config = $configTransformer->transform($env);
+//
+//    $weatherTypeTransformer = new WeatherTypeTransformer();
+//    $forecastTransformer = new ForecastTransformer($weatherTypeTransformer);
+//    $threeHourlyForecastApi = new ThreeHourlySiteForecastApi($config->getApiKey());
+//
+//    $dataProvider = new DataProvider($config, $forecastTransformer, $threeHourlyForecastApi);
+//    $cloudFunction = new CloudFunction($dataProvider, $config->getFunctionConfig());
+//    $response = $cloudFunction->run($request);
+//
+//    return $response;
 }
